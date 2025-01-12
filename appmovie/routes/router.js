57dd23a9367697e2820d2a538b60a5ab59a230aa
AbelHendrikMP <160741457+AbelHendrikMP@users.noreply.github.com>
2025-01-12 20:30:00 +0700

    TMDB assignment

---
+++ appmovie/routes/router.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express()
+const movieController = require('../controllers/movie-controller')
+
+router.get('/movie',movieController.searchMovie)
+router.get('/movie/popular',movieController.searchPopularMovie)
+
+module.exports = router
